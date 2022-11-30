f42a8cdb68c7b28422619b2bba9810771510acdf
osamadado123 <osama.aldado@gmail.com>
2022-11-30 23:24:18 +0000

    adding the tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class test_snack(TestCase):
+    def test_status (self):
+        url = reverse('snacks_list')
+        response = self.client.get(url)
+        
+        self.assertEqual(response.status_code, 200)
+
+    def test_template(self):
+        url = reverse('snacks_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
